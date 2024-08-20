03869d5d2a027a0edbc91742237abc9595c5d97c
lilimari01 <00001134259463sp@al.educacao.sp.gov.br>
2024-08-20 15:14:39 -0300

    adicionando sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createcanvas(600, 600);
+    background("black")
+}
+
+function draw() {
+
+    stroke("blue")
+    FileList("red")
+
+    //console.log(mouseIsPressed)
+
+    if (mouseIsPressed)
+    rect(mouseX,ouseY,20,45)
+}
